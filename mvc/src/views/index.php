c3852f0abb7fa9a3f8fbe81afd49d4f9efe07653
Adria Moya Duran <adriamoyaduran787@gmail.com>
2023-10-16 17:33:41 +0200

    modificaciones de la pagina de inicio

--- mvc/src/views/index.php
+++ mvc/src/views/index.php
@@ -5,11 +5,11 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Bootstrap demo</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="srcs/style.css">
    </head>
    <body>
-      <!-- sss -->
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+   <nav class="navbar navbar-expand-lg bg-body-tertiary">
          <div class="container-fluid">
             <img  class="img-thumbnail" alt="logo" width="auto" height="100px" style="">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,29 +24,20 @@
                <div style="float: right;">
                   <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                      <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Departamentos</a>
                      </li>
                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Link
+                        <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           Session
                         </a>
-                        <ul class="dropdown-menu">
-                           <li><a class="dropdown-item" href="#">Action</a></li>
-                           <li><a class="dropdown-item" href="#">Another action</a></li>
-                           <li>
-                              <hr class="dropdown-divider">
-                           </li>
-                           <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
                      </li>
-                     <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Link</a>
-                     </li>
+                   
                   </ul>
                </div>
             </div>
          </div>
       </nav>
+
       <!-- carrousel -->
       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
          <div class="carousel-inner">
@@ -69,7 +60,7 @@
          <span class="visually-hidden">Next</span>
          </button>
       </div>
-      <div class="container contendores_ini" >
+      <div class="container contendores_ini"style="margin-top:100px; margin-bottom:100px;" >
          <div class="row">
             <div class="col-3">
                <img src="https://hips.hearstapps.com/hmg-prod/images/casa-de-diseno-contemporaneo26-1637602658.jpg?crop=1xw:0.9066731141199227xh;center,top&resize=1200:*" class="d-block w-100" alt="casa blanca">
@@ -79,6 +70,7 @@
             </div>
          </div>
       </div>
+      
       <!-- info en medio -->
       <div class="container contendores_ini" >
          <div class="row">
@@ -96,7 +88,7 @@
             <div class="col-1">
             </div>
             <div class="col-10">
-               <h3 class="text-center" style="padding:20px;">Busca apartamentos</h3>
+               <h3 class="text-center" id="title_apartamentos" style="padding:50px;">Mira nuestros apartamentos</h3>
                <div class="container">
                   <div class="row">
                      <div class="col-4">
