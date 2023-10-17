306e36305f181853b2a9210dd90591995912c3b5
Marcelo Palma <dhernandez@cendrassos.net>
2023-10-17 19:33:10 +0200

    feature-session v5

--- mvc/src/controllers/index.php
+++ mvc/src/controllers/index.php
@@ -10,6 +10,7 @@
  **/
 
 /**
+<<<<<<< HEAD
  * ctrlIndex: Controlador que carrega les tasques i visaulitza la portada
  *
  * @param $get array associatiu amb el contingut de la variable $_GET.
@@ -22,3 +23,17 @@ function ctrlIndex()
 {
   include "../src/views/index.php";
 }
+=======
+  * ctrlIndex: Controlador que carrega les tasques i visaulitza la portada
+  *
+  * @param $get array associatiu amb el contingut de la variable $_GET.
+  * @param $cookie array associatiu amb el contingut de la variable $_COOKIE.
+  * @param $session Model que encapsula la sessió.
+  * @param $images Model que encapsula les imatges.
+  *
+**/
+function ctrlIndex(){
+  include "../src/views/index.php";
+}
+
+>>>>>>> 44621a6 (feature-session v5)
